558c72ea34b9415840964aa0b5f58cca12421419
wangqt93 <969909031@qq.com>
2020-07-20 13:17:09 +0800

    完善

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-function Dep(){
-    this = self
+function decoy(obj){
+    if(typeof obj !== 'object' || obj === null){
+        return obj
+    }else{
+        let arr2 = Array.isArray(obj) ? [] : {}
+        for(let key in obj){
+            if(typeof obj[key] === 'object' && obj[key] !== null){
+                arr2[key] = decoy(obj[key])
+            }else{
+                arr2[key] = obj[key]
+            }
+        }
+      
+        return arr2
+    }
+
 }
+
+arr = [1,3,[1,6]]
+console.log(decoy(arr))
+
